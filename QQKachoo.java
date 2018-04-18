3d7c8da635013e2db3a1e77712ac575f6168f0d5
Rubin Peci <rubinpeci@gmail.com>
2018-04-18 09:46:17 +0000

    Update QQKachoo

--- QQKachoo.java
+++ QQKachoo.java
@@ -9,5 +9,52 @@ public class QQKachoo<D> implements Deque<D> {
 	private ArrayList<D> _buba;
 	
 	public QQKachoo() {
+		_buba = new ArrayList<D>();
+	}
+
+	//constructor for constrained Deque
+	//to be completed later
+	public QQKachoo(int size) {
+		_buba = new ArrayList<D>(size);
+	}
+	
+	public void addFirst(D value) {
+		if (_buba.size() > 0)
+			_buba.add(0, value);
+		else
+			_buba.add(value);
+	} 
+	
+	public void addLast(D value) {
+		_buba.add(value);
+	}
+
+	public D removeFirst() {
+		return _buba.remove(0);
+	}
+
+	public D removeLast() {
+		return _buba.remove(_buba.size() - 1);
+	}
+
+	public D getFirst() {
+		return _buba.get(0);
+	}
+
+	public D getLast() {
+		return _buba.get(_buba.size() - 1);
+	}
+
+	@Override
+	public String toString() {
+		return _buba.toString();
+	}
+	public static void main (String[] args) {
+		Deque<String> gummo = new QQKachoo<String>();
+		gummo.addFirst("Yeet");
+		gummo.addFirst("Yote");
+		gummo.addLast("Yate");
+		System.out.println(gummo);
 	}
 }
+
