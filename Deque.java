c4dee0658d76437a3bbe24cfc0351e88649b29a4
Rubin Peci <rubinpeci@gmail.com>
2018-04-18 09:13:05 +0000

    Add Deque interface

---
+++ Deque.java
@@ -0,0 +1,20 @@
+//Wubin Peco, Max Millar, Bo Lu
+//APCS-2 pd02
+//LAB xx
+
+public interface Deque<Card> {
+	
+	public void addFirst(Card value);
+	
+	public void addLast(Card value);
+
+	public Card removeFirst();
+
+	public Card removeLast();
+
+	public Card getFirst();
+
+	public Card getLast();
+
+	//adding special value methods later
+}
